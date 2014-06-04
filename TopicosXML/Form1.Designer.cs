dcaf202f5d551e39c838564f61d4a5f97a57319d
stivencuellar <stivencuellar@gmail.com>
2014-06-04 16:12:50 -0500

    Segunda Version
    Segunda Version de Proyecto XML

---
+++ TopicosXML/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TopicosXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.grpDecorador = new System.Windows.Forms.GroupBox();
+            this.rtxtDecorador = new System.Windows.Forms.RichTextBox();
+            this.grpFuente = new System.Windows.Forms.GroupBox();
+            this.rtxtFuente = new System.Windows.Forms.RichTextBox();
+            this.grpResultado = new System.Windows.Forms.GroupBox();
+            this.rtxtResultado = new System.Windows.Forms.RichTextBox();
+            this.ttpXMLDecorator = new System.Windows.Forms.ToolTip(this.components);
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnCargarFuente = new System.Windows.Forms.Button();
+            this.btnCargarDecorador = new System.Windows.Forms.Button();
+            this.ofdXMLSelector = new System.Windows.Forms.OpenFileDialog();
+            this.grpDecorador.SuspendLayout();
+            this.grpFuente.SuspendLayout();
+            this.grpResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDecorador
+            // 
+            this.grpDecorador.Controls.Add(this.rtxtDecorador);
+            this.grpDecorador.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpDecorador.Location = new System.Drawing.Point(61, 37);
+            this.grpDecorador.Name = "grpDecorador";
+            this.grpDecorador.Size = new System.Drawing.Size(383, 453);
+            this.grpDecorador.TabIndex = 2;
+            this.grpDecorador.TabStop = false;
+            this.grpDecorador.Text = "DECORADOR";
+            // 
+            // rtxtDecorador
+            // 
+            this.rtxtDecorador.Location = new System.Drawing.Point(7, 26);
+            this.rtxtDecorador.Name = "rtxtDecorador";
+            this.rtxtDecorador.ReadOnly = true;
+            this.rtxtDecorador.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtxtDecorador.Size = new System.Drawing.Size(370, 419);
+            this.rtxtDecorador.TabIndex = 0;
+            this.rtxtDecorador.Text = "";
+            // 
+            // grpFuente
+            // 
+            this.grpFuente.Controls.Add(this.rtxtFuente);
+            this.grpFuente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpFuente.Location = new System.Drawing.Point(495, 37);
+            this.grpFuente.Name = "grpFuente";
+            this.grpFuente.Size = new System.Drawing.Size(383, 453);
+            this.grpFuente.TabIndex = 3;
+            this.grpFuente.TabStop = false;
+            this.grpFuente.Text = "FUENTE";
+            // 
+            // rtxtFuente
+            // 
+            this.rtxtFuente.Location = new System.Drawing.Point(0, 25);
+            this.rtxtFuente.Name = "rtxtFuente";
+            this.rtxtFuente.ReadOnly = true;
+            this.rtxtFuente.Size = new System.Drawing.Size(370, 419);
+            this.rtxtFuente.TabIndex = 1;
+            this.rtxtFuente.Text = "";
+            // 
+            // grpResultado
+            // 
+            this.grpResultado.Controls.Add(this.rtxtResultado);
+            this.grpResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpResultado.Location = new System.Drawing.Point(926, 37);
+            this.grpResultado.Name = "grpResultado";
+            this.grpResultado.Size = new System.Drawing.Size(383, 453);
+            this.grpResultado.TabIndex = 4;
+            this.grpResultado.TabStop = false;
+            this.grpResultado.Text = "RESULTADO";
+            // 
+            // rtxtResultado
+            // 
+            this.rtxtResultado.Location = new System.Drawing.Point(0, 25);
+            this.rtxtResultado.Name = "rtxtResultado";
+            this.rtxtResultado.ReadOnly = true;
+            this.rtxtResultado.Size = new System.Drawing.Size(370, 419);
+            this.rtxtResultado.TabIndex = 1;
+            this.rtxtResultado.Text = "";
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.BackgroundImage = global::TopicosXML.Properties.Resources.WMP11;
+            this.btnProcesar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnProcesar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProcesar.Location = new System.Drawing.Point(1098, 496);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(64, 58);
+            this.btnProcesar.TabIndex = 6;
+            this.ttpXMLDecorator.SetToolTip(this.btnProcesar, "Ejecutar Decorador");
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnCargarFuente
+            // 
+            this.btnCargarFuente.BackgroundImage = global::TopicosXML.Properties.Resources.Arrow_Up;
+            this.btnCargarFuente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCargarFuente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCargarFuente.Location = new System.Drawing.Point(666, 496);
+            this.btnCargarFuente.Name = "btnCargarFuente";
+            this.btnCargarFuente.Size = new System.Drawing.Size(67, 58);
+            this.btnCargarFuente.TabIndex = 5;
+            this.ttpXMLDecorator.SetToolTip(this.btnCargarFuente, "Cargar Fuente");
+            this.btnCargarFuente.UseVisualStyleBackColor = true;
+            this.btnCargarFuente.Click += new System.EventHandler(this.btnCargarFuente_Click);
+            // 
+            // btnCargarDecorador
+            // 
+            this.btnCargarDecorador.BackgroundImage = global::TopicosXML.Properties.Resources.Arrow_Up;
+            this.btnCargarDecorador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCargarDecorador.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCargarDecorador.Location = new System.Drawing.Point(222, 496);
+            this.btnCargarDecorador.Name = "btnCargarDecorador";
+            this.btnCargarDecorador.Size = new System.Drawing.Size(67, 58);
+            this.btnCargarDecorador.TabIndex = 0;
+            this.ttpXMLDecorator.SetToolTip(this.btnCargarDecorador, "Cargar Decorador");
+            this.btnCargarDecorador.UseVisualStyleBackColor = true;
+            this.btnCargarDecorador.Click += new System.EventHandler(this.btnCargarDecorador_Click);
+            // 
+            // ofdXMLSelector
+            // 
+            this.ofdXMLSelector.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1308, 568);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnCargarFuente);
+            this.Controls.Add(this.grpResultado);
+            this.Controls.Add(this.grpFuente);
+            this.Controls.Add(this.grpDecorador);
+            this.Controls.Add(this.btnCargarDecorador);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML DECORATOR";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.grpDecorador.ResumeLayout(false);
+            this.grpFuente.ResumeLayout(false);
+            this.grpResultado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargarDecorador;
+        private System.Windows.Forms.GroupBox grpDecorador;
+        private System.Windows.Forms.GroupBox grpFuente;
+        private System.Windows.Forms.GroupBox grpResultado;
+        private System.Windows.Forms.Button btnCargarFuente;
+        private System.Windows.Forms.ToolTip ttpXMLDecorator;
+        private System.Windows.Forms.RichTextBox rtxtDecorador;
+        private System.Windows.Forms.RichTextBox rtxtFuente;
+        private System.Windows.Forms.RichTextBox rtxtResultado;
+        private System.Windows.Forms.OpenFileDialog ofdXMLSelector;
+        private System.Windows.Forms.Button btnProcesar;
+    }
+}
+
---
+++ TopicosXML/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using System.Web;
+
+namespace TopicosXML
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument xmlDecoratorDoc = null;
+        XmlDocument xmlFuenteDoc = null;
+        XmlDocument xmlResultadoDoc = null;
+        string pathXmlDecorator = "";
+        string pathXmlFuente = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            xmlDecoratorDoc = new XmlDocument();
+            xmlFuenteDoc = new XmlDocument();
+            xmlResultadoDoc = new XmlDocument();
+        }
+
+        private void btnCargarDecorador_Click(object sender, EventArgs e)
+        {
+            cargarDecorador();
+        }
+
+        private void btnCargarFuente_Click(object sender, EventArgs e)
+        {
+            cargarFuente();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            if (rtxtDecorador.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese Decorador");
+            }
+            else if (rtxtFuente.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese Fuente");
+            }
+            else
+            {
+                validadorAccion();
+            }
+        }
+
+        private void validadorAccion()
+        {
+            XPathDocument document = new XPathDocument(pathXmlDecorator);
+            XPathNavigator navigator = document.CreateNavigator();
+            XmlNamespaceManager nameSpaceDecorator = new XmlNamespaceManager(navigator.NameTable);
+            nameSpaceDecorator.AddNamespace("cora", "http://www.xmlstairways.com/cora");
+
+            XmlNodeList listaNodoDecorator = xmlDecoratorDoc.SelectNodes("/cora:set/cora:decorator", nameSpaceDecorator);
+            IEnumerator iEnumDecorator = listaNodoDecorator.GetEnumerator();
+
+            while (iEnumDecorator.MoveNext())
+            {
+                XmlNode nodoDecorator = (XmlNode)iEnumDecorator.Current;
+                XmlElement elemDecorator = (XmlElement)nodoDecorator;
+                string atributoRef = (elemDecorator.GetAttribute("ref"));
+
+                XmlNodeList hijosDecorador = nodoDecorator.ChildNodes;
+                IEnumerator iEnumHijosDecorador = hijosDecorador.GetEnumerator();
+
+                while (iEnumHijosDecorador.MoveNext())
+                {
+                    XmlNode nodoHijo = (XmlNode)iEnumHijosDecorador.Current;
+                    string tipoModificacion = nodoHijo.LocalName;
+
+                    XmlNodeList nietosDecorador = nodoHijo.ChildNodes;
+                    IEnumerator iEnumNientosDecorador = nietosDecorador.GetEnumerator();
+
+                    while (iEnumNientosDecorador.MoveNext())
+                    {
+                        XmlNode nodoNieto = (XmlNode)iEnumNientosDecorador.Current;
+
+                        string elem = nodoNieto.Name;
+                        string ContElem = nodoNieto.InnerText;
+
+                        XmlElement newElement = xmlFuenteDoc.CreateElement(elem);
+                        newElement.InnerText = ContElem;
+
+                        XmlNode nodeFuente = xmlFuenteDoc.SelectSingleNode(atributoRef);
+                        XmlNode nodoPadre = xmlFuenteDoc.SelectSingleNode(atributoRef + "/..");
+
+                        if (tipoModificacion.Equals("append"))
+                        {
+                            nodeFuente.AppendChild(newElement);
+                        }
+                        if (tipoModificacion.Equals("insert"))
+                        {
+                            nodoPadre.InsertAfter(newElement, nodeFuente.FirstChild);
+                        }
+                        if (tipoModificacion.Equals("replace"))
+                        {
+                            nodoPadre.ReplaceChild(newElement, nodeFuente);
+                        }
+                    }
+                }
+            }
+
+            //Guardo el resultado y lo muestro!
+            string resul = "newDoc.xml";
+            xmlFuenteDoc.Save(resul);
+            xmlResultadoDoc.Load(resul);
+            rtxtResultado.LoadFile(resul, RichTextBoxStreamType.PlainText);
+        }
+
+        public void cargarDecorador()
+        {
+            ofdXMLSelector.Title = "Buscar Archivo XML DECORATOR";
+            ofdXMLSelector.Filter = "Archivos XML | decorator.xml";
+            if (ofdXMLSelector.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pathXmlDecorator = ofdXMLSelector.FileName;
+                rtxtDecorador.LoadFile(pathXmlDecorator, RichTextBoxStreamType.PlainText);
+                xmlDecoratorDoc.Load(pathXmlDecorator);
+            }
+        }
+
+        public void cargarFuente() 
+        {
+            ofdXMLSelector.Title = "Buscar Archivo XML SOURCE";
+            ofdXMLSelector.Filter = "Archivos XML | source.xml";
+            if (ofdXMLSelector.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pathXmlFuente = ofdXMLSelector.FileName;
+                rtxtFuente.LoadFile(pathXmlFuente, RichTextBoxStreamType.PlainText);
+                xmlFuenteDoc.Load(pathXmlFuente);
+            } 
+        }
+    }
+}
